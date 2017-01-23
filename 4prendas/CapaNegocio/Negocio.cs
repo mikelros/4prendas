f0b59bc3876c46946958067d1b48cc51b4093fb3
mikelros <mikelros22@gmail.com>
2017-01-23 11:45:14 +0100

    Comprobar control de código de barras y sacar control hecho.

--- 4prendas/CapaNegocio/Negocio.cs
+++ 4prendas/CapaNegocio/Negocio.cs
@@ -8,5 +8,48 @@ namespace CapaNegocio
 {
     public class Negocio
     {
+        public ulong SacarControl(ulong codBarras)
+        {
+            char[] codBarrasChar = codBarras.ToString().ToCharArray();
+            ulong impares = 0;
+            ulong pares = 0;
+
+            for (int i = 0; i < codBarrasChar.Length; i++)
+            {
+                ulong num = (ulong)Char.GetNumericValue(codBarrasChar[i]);
+
+                if (i % 2 == 0)
+                {
+                    pares += num;
+                }
+                else
+                {
+                    impares += num;
+                }
+            }
+
+            ulong imparesMult = impares * 3;
+            ulong suma = imparesMult + pares;
+
+            if (suma % 10 == 0)
+            {
+                return 0;
+            }
+
+            char[] sumaChar = suma.ToString().ToCharArray();
+            ulong control = 10 - (ulong)Char.GetNumericValue(sumaChar[sumaChar.Length - 1]);
+
+            return control;
+        }
+
+        public bool ComprobarControl(ulong codBarrasConControl)
+        {
+            ulong codBarras = UInt64.Parse(codBarrasConControl.ToString().Remove(codBarrasConControl.ToString().Length - 1));
+            ushort control = UInt16.Parse(codBarrasConControl.ToString().ToCharArray()[codBarrasConControl.ToString().Length - 1].ToString());
+
+            return SacarControl(codBarras) == control;
+        }
+    
     }
+
 }
