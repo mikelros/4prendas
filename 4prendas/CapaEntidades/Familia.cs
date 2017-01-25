09c9646825dccb427d4de3c44012397e21618507
DavoLarris <davo.larris@hotmail.com>
2017-01-25 12:19:15 +0100

    A mi no me chilleis

---
+++ 4prendas/CapaEntidades/Familia.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Familia
+    {
+        private String codFamilia;
+        private String nombre;
+        private String imagen;
+        private List<SubFamilia> subFamilias;
+
+        public Familia()
+        {
+
+        }
+
+        public Familia(String codFamilia, String nombre, String imagen, ArrayList<SubFamilia> subFamilias) 
+        {
+            this.codFamilia = codFamilia;
+            this.nombre = nombre;
+            this.imagen = imagen;
+            this.subFamilias = subFamilias;
+        }
+
+        public String CodFamilia
+        {
+            get
+            {
+                return codFamilia;
+            }
+            set
+            {
+                codFamilia = value;
+            }
+        }
+
+        public String Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public String Imagen
+        {
+            get
+            {
+                return imagen;
+            }
+            set
+            {
+                imagen = value;
+            }
+        }
+
+        public List<SubFamilia> SubFamilias
+        {
+            get
+            {
+                return subFamilias;
+            }
+            set
+            {
+                subFamilias = value;
+            }
+        }
+    }
+}
---
+++ 4prendas/CapaEntidades/Lugar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    class Lugar
+    {
+        private String estanteria;
+        private int estante;
+        private int altura;
+
+        public Lugar()
+        {
+
+        }
+
+        public Lugar(String estanteria, int estante, int altura)
+        {
+            this.estanteria = estanteria;
+            this.estante = estante;
+            this.altura = altura;
+        }
+
+        public String Estanteria
+        {
+            get
+            {
+                return estanteria;
+            }
+            set
+            {
+                estanteria = value;
+            }
+        }
+
+        public int Estante
+        {
+            get
+            {
+                return estante;
+            }
+            set
+            {
+                estante = value;
+            }
+        }
+
+        public int Altura
+        {
+            get
+            {
+                return altura;
+            }
+            set
+            {
+                altura = value;
+            }
+        }
+    }
+}
