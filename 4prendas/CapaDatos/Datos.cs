c608a97fe973b68718d5e80dc7082bd55f887be7
mikelros <mikelros22@gmail.com>
2017-01-25 12:07:05 +0100

    Métodos para sacar familias, subfamilias y productos (faltan clases e instanciamientos)

--- 4prendas/CapaDatos/Datos.cs
+++ 4prendas/CapaDatos/Datos.cs
@@ -3,10 +3,128 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace CapaDatos
 {
     public class Datos
     {
+        private static string cadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=4prendas.accdb";
+
+        public List<Familia> getFamilias()
+        {
+            List<Familia> familias = new List<Familia>(); // no sé si es arraylist o qué
+            string sql = "SELECT * FROM Familia";
+
+            OleDbConnection conTabla = new OleDbConnection(cadenaConexion);
+            OleDbCommand cmd = new OleDbCommand(sql, conTabla);
+
+            try
+            {
+                conTabla.Open();
+                OleDbDataReader dr = cmd.ExecuteReader();
+                if(!dr.HasRows)
+                {
+                    return familias; //sale vacía
+                }
+        
+          
+                while (dr.Read())
+                {
+                    //familias.Add(new Familia(dr.GetString(0), dr.GetString(1), dr.GetString(2), dr.GetValue(3))); // no tengo claro el orden ni los campos (de tener familia necesita join la sql)
+                }
+
+                return familias;
+                               
+            } catch (Exception ex)
+            {
+                //RaiseEvent errorBaseDatos(Me, New BaseDatosEventArgs("Error de base de datos"))
+                return null;
+            } finally
+            {
+                conTabla.Close();
+            }
+        }
+
+        //si la clase familia tiene subfamilias como propiedad getFamilias ya saca las subfamilias
+
+        //public List<Subfamilia> getSubfamilias(string codFamilia)
+        //{
+        //    List<Subfamilia> subfamilias = new List<Subfamilia>(); // no sé si es arraylist o qué
+        //    string sql = "SELECT * FROM Subfamilia WHERE Subfamilia.FamiliaCod = @codfamilia";
+
+        //    OleDbConnection conTabla = new OleDbConnection(cadenaConexion);
+        //    OleDbCommand cmd = new OleDbCommand(sql, conTabla);
+        //    cmd.Parameters.AddWithValue("@codfamilia", codFamilia);
+
+        //    try
+        //    {
+        //        conTabla.Open();
+        //        OleDbDataReader dr = cmd.ExecuteReader();
+        //        if (!dr.HasRows)
+        //        {
+        //            return subfamilias; //sale vacía
+        //        }
+
+
+        //        while (dr.Read())
+        //        {
+        //            //subfamilias.Add(new SubFamilia(dr.GetString(0), dr.GetString(1), dr.GetString(2))); // no tengo claro el orden ni los campos (de tener familia necesita join la sql)
+        //            //falta constructor y clases
+
+        //        }
+
+        //        return subfamilias;
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        //RaiseEvent errorBaseDatos(Me, New BaseDatosEventArgs("Error de base de datos"))
+        //        return null;
+        //    }
+        //    finally
+        //    {
+        //        conTabla.Close();
+        //    }
+        //}
+
+        public List<Producto> getProductos(string codSubfamilia)
+        {
+            List<Producto> productos = new List<Producto>(); // no sé si es arraylist o qué
+            string sql = "SELECT * FROM Productos WHERE Registro.CodSubFamilia = @codSubFamilia";
+
+            OleDbConnection conTabla = new OleDbConnection(cadenaConexion);
+            OleDbCommand cmd = new OleDbCommand(sql, conTabla);
+            cmd.Parameters.AddWithValue("@codSubFamilia", codSubfamilia);
+
+            try
+            {
+                conTabla.Open();
+                OleDbDataReader dr = cmd.ExecuteReader();
+                if (!dr.HasRows)
+                {
+                    return productos; //sale vacía
+                }
+
+
+                while (dr.Read())
+                {
+                    //subfamilia.Add(new Producto(dr.GetString(0), dr.GetString(1), dr.GetString(2), dr.GetValue(3))); // no tengo claro el orden ni los campos (de tener familia, subfamilia etc necesita join la sql)
+                }
+
+                return productos;
+
+            }
+            catch (Exception ex)
+            {
+                //RaiseEvent errorBaseDatos(Me, New BaseDatosEventArgs("Error de base de datos"))
+                return null;
+            }
+            finally
+            {
+                conTabla.Close();
+            }
+        }
+
     }
 }
